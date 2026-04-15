59e88ad8ed5697ec23008826238b340745ce8318
yulianoes <yulianoes24@gmail.com>
2026-04-15 09:36:27 +0100

    fix: reajuste 99% done

--- resources/views/errors/403.blade.php
+++ resources/views/errors/403.blade.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Acesso Negado - Bible Study</title>
-    <link rel="icon" type="image/png" href="{{ asset('favicon.png') }}">
+    <link rel="icon" type="image/png" href="/favicon.png">
     <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Outfit:wght@300;400;600&display=swap">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.min.css">
     <style>
